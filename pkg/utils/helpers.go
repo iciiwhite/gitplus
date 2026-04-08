ca96ff0e8f7205a648186bc8d7bcdd33b7a73473
Icii White <mail.iciiwhite@gmail.com>
2026-04-09 00:12:28 +0300

    Create new file

---
+++ pkg/utils/helpers.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func GenerateRandomString(n int) (string, error) {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
+
+func TruncateString(s string, maxLen int) string {
+	if len(s) <= maxLen {
+		return s
+	}
+	return s[:maxLen-3] + "..."
+}
